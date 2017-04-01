3e76a740f48c220ceca17c2e013fceba870da7f3
m1g <miguelmalcolm@outlook.com>
2017-03-31 22:06:27 -0400

    adds items and descriptions. stores locally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { IonicStorageModule } from '@ionic/storage';
+import { Data } from '../providers/data';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,17 +13,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
   ],
   providers: [
+    Data,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
+import { Data } from '../../providers/data';
 
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  public items = [];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
+    this.dataService.getData().then((todos) => {
+      if (todos) {
+        this.items = JSON.parse(todos)
+      }
+    })
+  }
+
+  ionViewDidLoad() {}
+
+  addItem() {
+
+    let addModal = this.modalCtrl.create(AddItemPage);
+
+    addModal.onDidDismiss((item) => {
+      if (item) {
+        this.saveItem(item);
+      }
+    });
+    addModal.present();
+  }
+
+  saveItem(item) {
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+
+  viewItem(item) {
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
   }
 
 }
